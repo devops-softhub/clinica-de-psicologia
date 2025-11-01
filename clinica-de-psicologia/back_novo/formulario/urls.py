05fe854b318ae72bb043bb5469c71c5a333fd6ce
Fabrica de Software <devops.softhub@unieuro.edu.br>
2025-11-01 00:58:27 +0000

    Limpando bacl

--- clinica-de-psicologia/back_novo/formulario/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views # Garante que está a importar o views.py do Passo 2
-
-app_name = 'formulario'
-
-urlpatterns = [
-    path('cadastro/comunidade/', views.formulario_comunidade_view, name='formulario_comunidade'),
-]
-
